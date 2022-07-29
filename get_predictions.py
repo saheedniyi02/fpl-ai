af5636d60c0321519d3a5d22309946f8c57c166f
saheedniyi02 <64325621+saheedniyi02@users.noreply.github.com>
2022-07-29 23:51:01 +0100

    Add files via upload

--- get_predictions.py
+++ get_predictions.py
@@ -15,11 +15,12 @@ def get_predictions(formation="442", clubs=None):
         predicted_defs = predicted_defs[predicted_defs["team_x"].isin(clubs)]
         predicted_mids = predicted_mids[predicted_mids["team_x"].isin(clubs)]
         predicted_gks = predicted_gks[predicted_gks["team_x"].isin(clubs)]
+        print(predicted_gks["name"])
     return {
-        "goalkeepers": predicted_gks["name"][1],
-        "defenders": predicted_defs["name"][: int(formation[0])],
-        "midfielders": predicted_mids["name"][: int(formation[1])],
-        "forwards": predicted_fwds["name"][: int(formation[2])],
+        "goalkeepers": list(predicted_gks["name"])[0],
+        "defenders": list(predicted_defs["name"])[: int(formation[0])],
+        "midfielders": list(predicted_mids["name"])[: int(formation[1])],
+        "forwards": list(predicted_fwds["name"])[: int(formation[2])],
     }
 
 
@@ -34,12 +35,12 @@ def get_full_squad():
     predicted_mids = pd.read_csv("predicted_dataset/midfielders_points.csv")
 
     return {
-        "goalkeepers": predicted_gks["name"][:2],
-        "defenders": predicted_defs["name"][:5],
-        "midfielders": predicted_mids["name"][:5],
-        "forwards": predicted_fwds["name"][:3],
+        "goalkeepers": list(predicted_gks["name"])[:2],
+        "defenders": list(predicted_defs["name"])[:5],
+        "midfielders": list(predicted_mids["name"])[:5],
+        "forwards": list(predicted_fwds["name"])[:3],
     }
 
 
-print(get_predictions(formation="442", clubs=None))
+print(get_predictions(formation="442", clubs=["Man City"]))
 print(get_full_squad())
